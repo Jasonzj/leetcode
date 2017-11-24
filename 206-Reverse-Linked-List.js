d3c7bbe3c9692a8d273307514ca2c77661f88b97
Jasonzj <925645402@qq.com>
2017-11-24 15:11:13 +0800

    [206-Reverse-Linked-List]: add

---
+++ 206-Reverse-Linked-List.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = function (head) {
+    let prev = null
+    let node = head
+    while (node) {
+        let next = node.next
+        node.next = prev
+        prev = node
+        node = next
+    }
+    return prev
+};
