31bde4987a6bdaa57c2b99c0df6c0f1ba84e1c1b
Jasonzj <925645402@qq.com>
2017-11-28 13:50:39 +0800

    [21-Merge-Two-Sorted-Lists]: add

---
+++ 21-Merge-Two-Sorted-Lists.js
@@ -0,0 +1,20 @@
+const mergeTwoLists = function (l1, l2) {
+    const result = new ListNode(0)
+    let curr = result
+
+    while (l1 !== null && l2 !== null) {
+        if (l1.val < l2.val) {
+            curr.next = l1
+            l1 = l1.next
+        } else {
+            curr.next = l2
+            l2 = l2.next
+        }
+        curr = curr.next
+    }
+
+    if (!l1) { curr.next = l2; }
+    if (!l2) { curr.next = l1; }
+
+    return result.next
+}
