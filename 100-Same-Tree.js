ecfaf9262cd38abec9b721ceb72adbba56613ce1
Jasonzj <925645402@qq.com>
2017-12-03 14:56:01 +0800

    [100-Same-Tree]: add

---
+++ 100-Same-Tree.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+const isSameTree = function (p, q) {
+    if (p == null && q == null) return true
+    if (p == null || q == null) return false
+    if (p.val == q.val)
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+    return false
+}
