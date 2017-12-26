14e149580442ae8609a7fe9bcce03064e3fb929c
Jasonzj <925645402@qq.com>
2017-12-26 16:03:57 +0800

    [145-Binary-Tree-Postorder-Traversal]: add

---
+++ 145-Binary-Tree-Postorder-Traversal.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const postorderTraversal = function(root) {
+    if (!root) return []
+    const result = []
+    const postOrder = function(node) {
+        if (!node) return
+        node.left && postOrder(node.left)
+        node.right && postOrder(node.right)
+        result.push(node.val)
+    }
+    postOrder(root)
+    return result
+}
