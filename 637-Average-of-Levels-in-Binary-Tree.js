9b0f64d6d9dd6e8372077bf115f89b7103271569
Jasonzj <925645402@qq.com>
2017-12-28 21:43:32 +0800

    [637-Average-of-Levels-in-Binary-Tree]: add

---
+++ 637-Average-of-Levels-in-Binary-Tree.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const averageOfLevels = function(root) {
+    if (!root) return []
+    const result = []
+    const preOrder = function(node, depth) {
+        if (!node) return
+        result[depth] ? result[depth].push(node.val) : result[depth] = [node.val]
+        node.left && preOrder(node.left, depth + 1)
+        node.right && preOrder(node.right, depth + 1)
+    }
+    preOrder(root, 0)
+    return result.map(arr => arr.reduce((a, b) => a + b, 0) / arr.length)
+}
+
+/* ---------------------------------------------------------------------------------- */
+
+const averageOfLevels = function (root) {
+    const result = []
+    const queue = [root]
+    if (!root) return result
+    while (queue.length) {
+        let n = queue.length
+        let sum = 0
+        for (let i = 0; i < n; i++) {
+            const node = queue.shift()
+            sum += node.val
+            node.left && queue.push(node.left)
+            node.right && queue.push(node.right)
+        }
+        result.push(sum / n)
+    }
+    return result
+}
